b0ee4e8378ca026d26eae1744243d0f0513622c9
Glebados <glebrudenko22@gmail.com>
2021-10-25 12:18:13 +0200

    Task complete

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -3,6 +3,6 @@ package it.campe;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("У тебя все получится!");
+        System.out.println("Я думаю, быть программистом круто");
     }
 }
